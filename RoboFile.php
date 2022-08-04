57247de62e1656225a49340bb92a8410d0443c29
Andrii Podanenko <podarokua@gmail.com>
2022-08-04 15:46:53 +0300

    fix: migrate to yusa

--- RoboFile.php
+++ RoboFile.php
@@ -15,7 +15,7 @@ class RoboFile extends \Robo\Tasks {
   function OpenyCreateProject($path) {
     $this->taskComposerCreateProject()
       ->source('ycloudyusa/yusaopeny-project:9.2.x-init-dev')
-      ->target($path . '/openy-project')
+      ->target($path . '/yusaopeny-project')
       ->ansi(TRUE)
       ->dev()
       ->noInstall(TRUE)
@@ -34,7 +34,7 @@ class RoboFile extends \Robo\Tasks {
    */
   function OpenyAddFork($path, $repository) {
     $this->taskComposerConfig()
-      ->dir($path . '/openy-project')
+      ->dir($path . '/yusaopeny-project')
       ->repository(99, $repository, 'path')
       ->ansi(TRUE)
       ->run();
@@ -52,7 +52,7 @@ class RoboFile extends \Robo\Tasks {
    */
   function OpenySetBranch($path, $branch) {
     $this->taskComposerRequire()
-      ->dir($path . '/openy-project')
+      ->dir($path . '/yusaopeny-project')
       ->dependency('ycloudyusa/yusaopeny', $branch)
       ->dev()
       ->ansi(TRUE)
@@ -67,7 +67,7 @@ class RoboFile extends \Robo\Tasks {
    */
   function OpenyInstall($path) {
     $this->taskComposerInstall()
-      ->dir($path . '/openy-project')
+      ->dir($path . '/yusaopeny-project')
       ->noInteraction()
       ->dev()
       ->ansi(TRUE)
